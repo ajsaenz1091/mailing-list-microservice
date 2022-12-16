846ec40a2f387db70fb467aa5f0c467c22e73474
Alberto Gonzalez <ajsaenz1091@gmail.com>
2022-12-15 22:35:17 -0500

    implemented crud operations

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -4,14 +4,15 @@ import (
 	"database/sql"
 	"log"
 	"time"
+
 	"github.com/mattn/go-sqlite3"
 )
 
 type EmailEntry struct {
-	Id int64
-	Email string
+	Id          int64
+	Email       string
 	ConfirmedAt *time.Time
-	OptOut bool
+	OptOut      bool
 }
 
 func TryCreate(db *sql.Db) {
@@ -54,4 +55,87 @@ func emailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 	// convert time to apropriate time structure
 	t := time.Unix(confirmedAt, 0)
 	return &EmailEntry{Id: id, Email: email, ConfirmedAt: &t, OptOut: optOut}, nil
+}
+
+// CRUD OPERATIONS
+
+// CREATE EMAIL
+
+func CreateEmail(db *sql.DB, email string) error {
+	query := `INSERT INTO
+		emails(email, confirmed_at, opt_out)
+		VALUES(?, 0, false)`
+	_, err := db.Exec(query, email)
+	// check for errors
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+// READ EMAIL
+
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	query := `SELECT id, email, confirmed_at, opt_out
+		FROM emails
+		WHERE email = ?`
+	// the data returned by the Query method is returned as rows
+	rows, err := db.Query(query, email)
+	// check for errors
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	// the Query methd keeps the db open so it can keep reading more rows so we need to defer closing it to freexe up the db
+	defer rows.Close()
+
+	// iterate through the rows and return an EmailEntry from row
+	// rows.Next returns a sql.Rows data type which we pass to our emailEntryFromRow function to create a new EmailEntry
+	for rows.Next() {
+		return emailEntryFromRow(rows)
+	}
+	return nil, nil
+}
+
+// UPDATE EMAIL
+
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	t := entry.ConfirmedAt.Unix()
+	query := `INSET INTO
+		emails(email, confirmed_at, opt_out)
+		VALUES(?, ?, ?)
+		ON CONFLICT(email) DO UPDATE SET
+			confirmed_at=?
+			opt_out=?
+		`
+	_, err := db.Exec(query, entry.Email, t, entry.OptOut, t, entry.OptOut)
+	// check for errors
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return nil
+}
+
+// DELETE EMAIL
+
+func DeleteEmail(db *sql.DB, email string) error {
+	// Normally a delete operation would delete a row from db like so:
+	// query := `DELETE FROM emails
+	// 	WHERE email=?
+	// `
+	// But since this app is a mailing list, we just want to update the opt_out field to be true
+	query := `UPDATE emails
+			  SET opt_out=true
+			  WHERE email=?`
+	_, err := db.Exec(query, email)
+	// check for errors
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return nil
 }
